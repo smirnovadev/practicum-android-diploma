3a468f49b1bb71665c56f5752461aa4f28161086
StasEvstigneev <evstigneevstas@gmail.com>
2024-06-07 21:08:30 +0300

    Создал класс App, прописал его в Манифесте

--- app/src/main/java/ru/practicum/android/diploma/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/App.kt
@@ -2,7 +2,7 @@ package ru.practicum.android.diploma
 
 import android.app.Application
 
-class App: Application() {
+class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
