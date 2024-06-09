8cb49db7facfa03df8aa370341a8b39c00a4b9e8
smirnovadev <annaparfenova514@gmail.com>
2024-06-09 21:25:08 +0300

    инициализировали в dataModule Koin

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,7 +1,13 @@
 package ru.practicum.android.diploma.di
 
+import android.content.Context
+import android.content.SharedPreferences
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val dataModule = module {
-
+    single<SharedPreferences> {
+        androidContext()
+            .getSharedPreferences("key_for_job_search_apps", Context.MODE_PRIVATE)
+    }
 }
