4d0424544bf2651b729bfb829fbfdb5c4c338cc2
StasEvstigneev <evstigneevstas@gmail.com>
2024-06-10 00:54:28 +0300

    Доработка экрана Поиск. Создал ViewModel, ScreenState. Подключил TextWatcher.

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,7 +1,12 @@
 package ru.practicum.android.diploma.di
 
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import ru.practicum.android.diploma.search.presentation.SearchViewModel
 
 val dataModule = module {
+    viewModel<SearchViewModel> {
+        SearchViewModel()
+    }
 
 }
