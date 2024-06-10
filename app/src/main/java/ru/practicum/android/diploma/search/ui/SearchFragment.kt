292ff16ec1f5a0c93fa4f6a25f04e597985d3ba6
StasEvstigneev <evstigneevstas@gmail.com>
2024-06-10 16:10:38 +0300

    Доработка экрана Search.

--- app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
@@ -37,35 +37,31 @@ class SearchFragment : Fragment() {
             renderState(state)
         }
 
-        binding.searchFieldIcon.setOnClickListener {
-            binding.editText.setText("")
-            viewModel.clearSearchField()
-        }
-
         textWatcher = object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO()
-            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                TODO()
-            }
-
-            override fun afterTextChanged(s: Editable?) {
                 if (!s.isNullOrEmpty()) {
                     binding.searchFieldIcon.setImageResource(R.drawable.ic_close)
                 } else {
                     binding.searchFieldIcon.setImageResource(R.drawable.ic_search)
                 }
             }
+
+            override fun afterTextChanged(s: Editable?) {}
         }
 
-        textWatcher.let { binding.editText.addTextChangedListener(it) }
+        textWatcher.let { binding.searchField.addTextChangedListener(it) }
+
+        binding.searchFieldIcon.setOnClickListener {
+            binding.searchField.setText("")
+            viewModel.clearSearchField()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        textWatcher.let { binding.editText.removeTextChangedListener(it) }
+        textWatcher.let { binding.searchField.removeTextChangedListener(it) }
         _binding = null
     }
 
@@ -74,9 +70,7 @@ class SearchFragment : Fragment() {
             is SearchScreenState.Default -> {
                 binding.searchScreenCover.isVisible = true
                 binding.progressBar.isVisible = false
-                binding.noInternetPlaceholder.isVisible = false
-                binding.emptySearchResultsPlaceholder.isVisible = false
-                binding.serverErrorPlaceholder.isVisible = false
+                binding.errorPlaceholder.isVisible = false
                 binding.searchStatus.isVisible = false
                 binding.recyclerView.isVisible = false
             }
@@ -94,14 +88,7 @@ class SearchFragment : Fragment() {
             }
 
             is SearchScreenState.SearchError -> {
-                binding.searchScreenCover.isVisible = false
-                binding.progressBar.isVisible = false
-                binding.noInternetPlaceholder.isVisible = false
-                binding.emptySearchResultsPlaceholder.isVisible = true
-                binding.serverErrorPlaceholder.isVisible = false
-                binding.searchStatus.isVisible = true
-                binding.searchStatus.text = getString(R.string.no_such_vacancies)
-                binding.recyclerView.isVisible = false
+                showSearchError()
             }
         }
     }
@@ -109,9 +96,7 @@ class SearchFragment : Fragment() {
     private fun showProgressbar() {
         binding.searchScreenCover.isVisible = false
         binding.progressBar.isVisible = true
-        binding.noInternetPlaceholder.isVisible = false
-        binding.emptySearchResultsPlaceholder.isVisible = false
-        binding.serverErrorPlaceholder.isVisible = false
+        binding.errorPlaceholder.isVisible = false
         binding.searchStatus.isVisible = false
         binding.recyclerView.isVisible = false
     }
@@ -119,9 +104,14 @@ class SearchFragment : Fragment() {
     private fun showInternetConnectionError() {
         binding.searchScreenCover.isVisible = false
         binding.progressBar.isVisible = false
-        binding.noInternetPlaceholder.isVisible = true
-        binding.emptySearchResultsPlaceholder.isVisible = false
-        binding.serverErrorPlaceholder.isVisible = false
+        binding.errorPlaceholder.isVisible = true
+        binding.errorPlaceholder.setText(R.string.no_internet)
+        binding.errorPlaceholder.setCompoundDrawablesWithIntrinsicBounds(
+            0,
+            R.drawable.img_internet_connection_error,
+            0,
+            0
+        )
         binding.searchStatus.isVisible = false
         binding.recyclerView.isVisible = false
     }
@@ -129,10 +119,31 @@ class SearchFragment : Fragment() {
     private fun showServerError() {
         binding.searchScreenCover.isVisible = false
         binding.progressBar.isVisible = false
-        binding.noInternetPlaceholder.isVisible = false
-        binding.emptySearchResultsPlaceholder.isVisible = false
-        binding.serverErrorPlaceholder.isVisible = true
+        binding.errorPlaceholder.isVisible = true
+        binding.errorPlaceholder.setText(R.string.server_error)
+        binding.errorPlaceholder.setCompoundDrawablesWithIntrinsicBounds(
+            0,
+            R.drawable.img_server_error_placeholder,
+            0,
+            0
+        )
         binding.searchStatus.isVisible = false
+        binding.recyclerView.isVisible = false
+    }
+
+    private fun showSearchError() {
+        binding.searchScreenCover.isVisible = false
+        binding.progressBar.isVisible = false
+        binding.errorPlaceholder.isVisible = true
+        binding.errorPlaceholder.setText(R.string.empty_search_results)
+        binding.errorPlaceholder.setCompoundDrawablesWithIntrinsicBounds(
+            0,
+            R.drawable.img_empty_search_results,
+            0,
+            0
+        )
+        binding.searchStatus.isVisible = true
+        binding.searchStatus.text = getString(R.string.no_such_vacancies)
         binding.recyclerView.isVisible = false
     }
 }
