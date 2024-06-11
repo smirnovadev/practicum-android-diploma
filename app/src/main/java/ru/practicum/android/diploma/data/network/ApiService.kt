034ac8d8dac9c30a96833c218303c3d9fb61aa95
nickSwany <Thief.leroy@yandex.ru>
2024-06-11 17:21:02 +0300

    Экран "Вакансия". Скрыл почту в Api

--- app/src/main/java/ru/practicum/android/diploma/data/network/ApiService.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/ApiService.kt
@@ -13,14 +13,14 @@ import ru.practicum.android.diploma.data.dto.responses.industry.IndustriesList
 interface ApiService {
     @Headers(
         "Authorization: Bearer ${BuildConfig.HH_ACCESS_TOKEN}",
-        "HH-User-Agent: YP HH Diploma (evstigneevstas@gmail.com)"
+        "HH-User-Agent: YP HH Diploma ({${BuildConfig.EMAIL})"
     )
     @GET("/vacancies/{vacancy_id}")
     suspend fun getVacancyById(@Path("vacancy_id") id: String): VacancyByIdResponse
 
     @Headers(
         "Authorization: Bearer ${BuildConfig.HH_ACCESS_TOKEN}",
-        "HH-User-Agent: YP HH Diploma (evstigneevstas@gmail.com)"
+        "HH-User-Agent: YP HH Diploma ({${BuildConfig.EMAIL})"
     )
     @GET("/vacancies")
     suspend fun findVacancies(
