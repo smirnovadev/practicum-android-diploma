33f14dc43f41a71831deb61c07a26cfa54db2935
StasEvstigneev <evstigneevstas@gmail.com>
2024-06-11 10:56:57 +0300

    Utils. Устранил замечания "detekt"

--- app/src/main/java/ru/practicum/android/diploma/util/Formatter.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/Formatter.kt
@@ -2,14 +2,24 @@ package ru.practicum.android.diploma.util
 
 object Formatter {
 
-    /** Функция для склонения существительных с числительным.
-    В конструктор передаются:
-    quantity - количество (Int);
-    singular - форма существительного в единственном числе (напр. "вакансия");
-    singularGenitive - единственное число, родительный падеж (напр. "вакансии");
-    pluralGenitive - множественное число, родительный падеж (напр. "вакансий").
-    Все три формы берутся из ресурсов strings и передаются в конструктор в формате строки String.
-    В качестве результата возвращается готовая форма с числительным (напр. "235 вакансий"). **/
+    const val ZERO = 0
+    const val TWO = 2
+    const val FOUR = 4
+    const val FIVE = 5
+    const val NINE = 9
+    const val ELEVEN = 11
+    const val NINETEEN = 19
+
+    /**
+     * Функция для склонения существительных с числительным.
+     * В конструктор передаются:
+     * quantity - количество (Int);
+     * singular - форма существительного в единственном числе (напр. "вакансия");
+     * singularGenitive - единственное число, родительный падеж (напр. "вакансии");
+     * pluralGenitive - множественное число, родительный падеж (напр. "вакансий").
+     * Все три формы берутся из ресурсов strings и передаются в конструктор в формате строки String.
+     * В качестве результата возвращается готовая форма с числительным (напр. "235 вакансий").
+     */
     fun quantityWordFormFormatter(
         quantity: Int,
         singular: String,
@@ -22,14 +32,14 @@ object Formatter {
         var wordForm: String
 
         when (resultLastTwo) {
-            in 11..19 -> {
+            in ELEVEN..NINETEEN -> {
                 wordForm = pluralGenitive
             }
 
             else -> {
-                if (resultLastOne == 0 || resultLastOne in 5..9) {
+                if (resultLastOne == ZERO || resultLastOne in FIVE..NINE) {
                     wordForm = pluralGenitive
-                } else if (resultLastOne in 2..4) {
+                } else if (resultLastOne in TWO..FOUR) {
                     wordForm = singularGenintive
                 } else {
                     wordForm = singular
