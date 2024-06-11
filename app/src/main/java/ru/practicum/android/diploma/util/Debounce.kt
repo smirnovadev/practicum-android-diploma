91ee176d95ef830010bb2c2a8a1361fef0a52684
StasEvstigneev <evstigneevstas@gmail.com>
2024-06-11 11:21:52 +0300

    Debounce. Исправил замечания Detekt.

--- app/src/main/java/ru/practicum/android/diploma/util/Debounce.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/Debounce.kt
@@ -3,10 +3,12 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-fun <T> debounce(delayMillis: Long,
-                 coroutineScope: CoroutineScope,
-                 useLastParam: Boolean,
-                 action: (T) -> Unit): (T) -> Unit {
+fun <T> debounce(
+    delayMillis: Long,
+    coroutineScope: CoroutineScope,
+    useLastParam: Boolean,
+    action: (T) -> Unit
+): (T) -> Unit {
     var debounceJob: Job? = null
     return { param: T ->
         if (useLastParam) {
