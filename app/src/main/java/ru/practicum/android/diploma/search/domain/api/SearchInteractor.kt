8be95b1ffccfa81ac3830d27007f4e1c0d379838
smirnovadev <annaparfenova514@gmail.com>
2024-06-12 12:39:01 +0300

    исправили замечание detect

--- app/src/main/java/ru/practicum/android/diploma/search/domain/api/SearchInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/api/SearchInteractor.kt
@@ -1,8 +1,9 @@
 package ru.practicum.android.diploma.search.domain.api
 
 import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.data.network.Resource
 import ru.practicum.android.diploma.search.domain.model.VacancySnippet
 
 interface SearchInteractor {
-    fun getVacancies(query: String): Flow<List<VacancySnippet>?>
+    fun getVacancies(query: String): Flow<Resource<List<VacancySnippet>>>
 }
--- app/src/main/java/ru/practicum/android/diploma/search/presentation/SearchViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/presentation/SearchViewModel.kt
@@ -3,13 +3,9 @@ package ru.practicum.android.diploma.search.presentation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.launch
-import ru.practicum.android.diploma.search.domain.api.SearchInteractor
 import ru.practicum.android.diploma.search.domain.model.VacancySnippet
 
 class SearchViewModel(
-    searchInteractor: SearchInteractor
 ) : ViewModel() {
 
     private var previousRequest: String = ""
@@ -20,11 +16,6 @@ class SearchViewModel(
     fun getVacanciesLiveData(): LiveData<List<VacancySnippet>> = vacanciesLiveData
 
     init {
-        viewModelScope.launch {
-            searchInteractor.getVacancies("Андроид-разработчик").collect {
-                vacanciesLiveData.postValue(it)
-            }
-        }
         screenState.postValue(SearchScreenState.Default)
     }
 
