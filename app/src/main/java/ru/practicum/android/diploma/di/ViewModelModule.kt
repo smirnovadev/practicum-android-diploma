3f3bd812b18da21a4a16a0dff2d1f73b385fe14f
Olga <ola.mk@mail.ru>
2024-06-14 04:40:55 +0300

    DIPLOM-73: Верстка экранов с фильтраим

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -3,7 +3,11 @@ package ru.practicum.android.diploma.di
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.favorites.presentation.FavoritesViewModel
+import ru.practicum.android.diploma.filters.presentation.CountryViewModel
 import ru.practicum.android.diploma.filters.presentation.FiltersViewModel
+import ru.practicum.android.diploma.filters.presentation.IndustryViewModel
+import ru.practicum.android.diploma.filters.presentation.PlaceToWorkViewModel
+import ru.practicum.android.diploma.filters.presentation.RegionViewModel
 import ru.practicum.android.diploma.job.ui.viewmodel.JobViewModel
 import ru.practicum.android.diploma.search.presentation.SearchViewModel
 
@@ -22,5 +26,17 @@ val viewModelModule = module {
 
     viewModel<FiltersViewModel> {
         FiltersViewModel()
+    }
+    viewModel<PlaceToWorkViewModel> {
+        PlaceToWorkViewModel()
+    }
+    viewModel<RegionViewModel> {
+        RegionViewModel()
+    }
+    viewModel<CountryViewModel> {
+        CountryViewModel()
+    }
+    viewModel<IndustryViewModel> {
+        IndustryViewModel()
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/filters/ui/PlaceToWorkFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/ui/PlaceToWorkFragment.kt
@@ -5,9 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentPlaceToWorkBinding
+import ru.practicum.android.diploma.filters.presentation.PlaceToWorkViewModel
 
 class PlaceToWorkFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    private var _binding: FragmentPlaceToWorkBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel by viewModel<PlaceToWorkViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPlaceToWorkBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.toolbar.setOnClickListener {
+            findNavController().navigateUp()
+        }
+        binding.countryText.setOnClickListener{
+            findNavController().navigate(R.id.action_placeToWorkFragment_to_countryFragment)
+        }
+        binding.regionText.setOnClickListener {
+            findNavController().navigate(R.id.action_placeToWorkFragment_to_regionFragment)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
 }
